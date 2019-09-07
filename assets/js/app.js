c06b476311b74b54cb62e680624418c9e9cb9a31
snamburi3 <sandeep.namburi@jax.org>
2019-09-07 19:18:26 -0400

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,7 +9,9 @@ app.controller('filterCtrl', ['$scope', function (scope) {
     scope.rowCollection = [
         {firstName: 'Laurent', lastName: 'Renard', birthDate: new Date('1987-05-21'), balance: 102, email: 'whatever@gmail.com'},
         {firstName: 'Blandine', lastName: 'Faivre', birthDate: new Date('1987-04-25'), balance: -2323.22, email: 'oufblandou@gmail.com'},
-        {firstName: 'Francoise', lastName: 'Frere', birthDate: new Date('1955-08-27'), balance: 42343, email: 'raymondef@gmail.com'}
+        {firstName: 'Francoise', lastName: 'Frere', birthDate: new Date('1955-08-27'), balance: 42343, email: 'raymondef@gmail.com'},
+        {firstName: 'Sandeep', lastName: 'Frere', birthDate: new Date('1955-08-27'), balance: 42343, email: 'raymondef@gmail.com'}
+
     ];
 
     scope.columnCollection = [

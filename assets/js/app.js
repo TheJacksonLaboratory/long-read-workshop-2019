5301338785092045f3f128381001241158003ff3
snamburi3 <sandeep.namburi@jax.org>
2019-09-07 23:45:07 -0400

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -69,7 +69,7 @@ app.controller('filterCtrl', ['$scope', '$filter', function (scope, filter) {
         {firstName: 'Blandine', lastName: 'Faivre', birthDate: new Date('1987-04-25'), balance: -2323.22, email: 'oufblandou@gmail.com'},
         {firstName: 'Francoise', lastName: 'Frere', birthDate: new Date('1955-08-27'), balance: 42343, email: 'raymondef@gmail.com'}
     ];
-
-    scope.predicates = ['firstName', 'lastName', 'birthDate', 'balance', 'email'];
-    scope.selectedPredicate = scope.predicates[0];
+    scope.globalConfig = {
+	        isGlobalSearchActivated: true
+    };
 }]);

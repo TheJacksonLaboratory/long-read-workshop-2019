5bc417f5c5daa063ef07fe5f6fb701785591c652
snamburi3 <sandeep.namburi@jax.org>
2019-09-07 23:37:31 -0400

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -81,3 +81,13 @@ app.controller('filterCtrl', ['$scope', function (scope) {
 }]);
 
 
+app.controller('filterCtrl', ['$scope', '$filter', function (scope, filter) {
+    scope.rowCollection = [
+        {firstName: 'Laurent', lastName: 'Renard', birthDate: new Date('1987-05-21'), balance: 102, email: 'whatever@gmail.com'},
+        {firstName: 'Blandine', lastName: 'Faivre', birthDate: new Date('1987-04-25'), balance: -2323.22, email: 'oufblandou@gmail.com'},
+        {firstName: 'Francoise', lastName: 'Frere', birthDate: new Date('1955-08-27'), balance: 42343, email: 'raymondef@gmail.com'}
+    ];
+
+    scope.predicates = ['firstName', 'lastName', 'birthDate', 'balance', 'email'];
+    scope.selectedPredicate = scope.predicates[0];
+}]);

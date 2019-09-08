66da586e09b3bd87ddcbea7671e3f68ffd457daf
snamburi3 <sandeep.namburi@jax.org>
2019-09-08 00:01:05 -0400

    changed to html

--- assets/js/app.js
+++ assets/js/app.js
@@ -62,14 +62,15 @@ app.directive('scrollTreshold', ['$window', function (window) {
 
 
 
-
 app.controller('filterCtrl', ['$scope', '$filter', function (scope, filter) {
     scope.rowCollection = [
         {firstName: 'Laurent', lastName: 'Renard', birthDate: new Date('1987-05-21'), balance: 102, email: 'whatever@gmail.com'},
         {firstName: 'Blandine', lastName: 'Faivre', birthDate: new Date('1987-04-25'), balance: -2323.22, email: 'oufblandou@gmail.com'},
         {firstName: 'Francoise', lastName: 'Frere', birthDate: new Date('1955-08-27'), balance: 42343, email: 'raymondef@gmail.com'}
     ];
+
     scope.globalConfig = {
-	        isGlobalSearchActivated: true
+            isGlobalSearchActivated: true
     };
 }]);
+
